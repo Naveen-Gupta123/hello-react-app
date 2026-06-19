8889b10893e2783086613e5b0fbe217e6ae66af1
Naveen Gupta <naveengupta44787@gmail.com>
2026-06-19 12:13:46 +0530

    Added Welcome component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import Welcome from "./Welcome";
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello React!</h1>
-      <p>My first React program.</p>
+    <div>
+      <Welcome />
     </div>
   );
 }
